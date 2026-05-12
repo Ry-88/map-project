d9d242bb0d4e4ea3f3f8a3f8f6ebdf100ea1f5cd
Rayan Hantoul <ryan-055@outlook.com>
2026-05-12 13:57:17 +0300

    run laravel

--- laravel-app/resources/views/navbar.blade.php
+++ laravel-app/resources/views/navbar.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="ar" dir="rtl">
 <head>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -15,10 +15,11 @@
     </style>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light mb-3">
-  <div class="container-fluid d-flex justify-content-center">
-    <img src="logo.png" class="logo" alt="logo">
-  </div>
-</nav>
 </body>
-</html>
+</html> --}}
+
+<nav class="navbar navbar-expand-lg navbar-light mb-3">
+<div class="container-fluid d-flex justify-content-center">
+<img src="{{ asset('images/logo2.png') }}" class="logo" alt="logo">
+</div>
+</nav>
